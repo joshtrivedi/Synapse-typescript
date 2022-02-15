03834c8ebb462958d019370699f7279e97e41729
Josh Trivedi <josh.trivedi@gmail.com>
2022-02-15 13:31:35 +0530

    cloned boilerplate

---
+++ environment.d.ts
@@ -0,0 +1,11 @@
+declare global {
+    namespace NodeJS {
+        interface ProcessEnv {
+            botToken: string,
+            guildId: string,
+            environments: "dev" | "prod" | "debug";
+        }
+    }
+}
+
+export {};
