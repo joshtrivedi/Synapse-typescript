c79f8cd6337d6847ffc3597765ec2b24eb46132c
Josh Trivedi <josh.trivedi@gmail.com>
2022-04-07 13:29:15 +0530

    chore: cleared test command

--- src/commands/info/test.ts
+++ src/commands/info/test.ts
@@ -2,36 +2,17 @@ import { Command } from '../../structures/Command'
 import Discord from 'discord.js'
 import { isAuthorModerator } from '../../permissionsHandler'
 import { embedCreate } from '../../structures/EmbedCreate'
-import fetch from 'node-fetch'
 
-const url = "https://www.aviationweather.gov/"
-const testApi = "https://www.aviationweather.gov/cgi-bin/json/TafJSON.php"
-const checkColor = (color: string): boolean => {
-    const reg = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/
-    return reg.test(color)
-}
 
 export default new Command({
     name: "test",
-    description: "testing command",
+    description: "command to fetch TAF data for an id",
     options: [
         {
-            name: "color",
-            description: "color",
-            required: false,
-            type: Discord.Constants.ApplicationCommandOptionTypes.STRING
-        },
-        {
-            name: "title",
-            description: "embed title",
-            required: false,
-            type: Discord.Constants.ApplicationCommandOptionTypes.STRING
-        },
-        {
-            name: "description",
-            description: "embed description",
-            required: false,
-            type: Discord.Constants.ApplicationCommandOptionTypes.STRING
+            name: "id",
+            description: `Put in the ID for Taf`,
+            required: true,
+            type: Discord.Constants.ApplicationCommandOptionTypes.STRING,
         }
     ],
     run: async ({ client, interaction }) => {
@@ -39,18 +20,11 @@ export default new Command({
             return interaction.followUp("not a mod")
         }
 
-        const response = await fetch(testApi)
+        const testTitles = ["1", "2", "3", "4", "5", "6", "7"]
+        const testValues = ["one", "two", "three", "four", "five", "six", "seven"]
 
-        if (response.status === 200) {
-            const title = await response.json()
-            Object.entries(title).forEach(([key, value]) => console.log(key,value))
-        }
-
-        // const args: string[] = [
-        //     interaction.options.getString('color') ?? "#ffffff",
-        //     interaction.options.getString('title') ?? "Please provider a title",
-        //     interaction.options.getString('description') ?? "Please provide a description",]
-        // interaction.followUp({ embeds: [embedCreate(args)] })
+        const embed = await embedCreate(["Test Embed", "Testing the Embed function"], testTitles, testValues)
+        interaction.followUp({embeds: [embed]})
 
     }
-})
+}) 
