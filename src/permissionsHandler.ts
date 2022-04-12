44b1bac71330a3510aae6fdf3f50fcbae6bc670c
Josh Trivedi <josh.trivedi@gmail.com>
2022-04-12 17:48:18 +0530

    enhancement: added permissions handler for the commands

--- src/permissionsHandler.ts
+++ src/permissionsHandler.ts
@@ -1,6 +1,10 @@
-import { Client, Guild, GuildMember, Message, MessageEmbed, PartialGuildMember, TextChannel, Interaction } from "discord.js"
-
-
+import { Client, Guild, GuildMember, Message, MessageEmbed, PartialGuildMember, TextChannel, Interaction, ClientApplication, ApplicationCommandDataResolvable, ApplicationCommandPermissionData } from "discord.js"
+import { promisify } from "util";
+import { Command } from "./structures/Command";
+import { glob } from "glob";
+import { CommandType } from "./typings/Command";
+import { client } from "./index";
+const globPromise = promisify(glob);
 export const isAuthorModerator = (member: GuildMember): boolean => {
     if (member.roles.hoist) {
         return member.roles.hoist.name === process.env.MODERATOR_ROLE_NAME;
@@ -22,8 +26,23 @@ export const isRegistered = (
     return !!member.roles.cache.find((r) => r.id === process.env.MEMBER_ROLE_ID);
 };
 
-export const isTimedout = ( 
+export const isTimedout = (
     member: GuildMember, PartialGuildMember
 ): boolean => {
     return !!member.roles.cache.find((r) => r.id === process.env.TIMEOUT_ROLE_ID);
+}
+
+
+export async function slashCommandsPermissions(id: string) {
+    const command = await client.guilds.cache?.get(process.env.guildId)?.commands.fetch(id);
+    const permissions: ApplicationCommandPermissionData[] =
+        [
+            {
+                id: '948267034775523403',
+                type: 'ROLE',
+                permission: true
+            },
+        ];
+    
+    await command.permissions.add({permissions});
 }
