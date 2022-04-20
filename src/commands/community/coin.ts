fda51809601d2e9201bd39c7ffdd769b609ee1c1
Josh Trivedi <josh.trivedi@gmail.com>
2022-04-20 16:25:44 +0530

    feat: function to flip a coin

---
+++ src/commands/community/coin.ts
@@ -0,0 +1,16 @@
+import { Command } from "../../structures/Command";
+import Discord, { Interaction } from "discord.js";
+
+export default new Command({
+    name: "flip",
+    description: "Flip a coin",
+    run: async ({ client, interaction }) => {
+        const channel: Discord.TextChannel = interaction.channel as Discord.TextChannel
+        const coin = Math.floor(Math.random() * 2)
+        if (coin === 0) {
+            await interaction.followUp("Heads")
+        } else {
+            await interaction.followUp("Tails")
+        }
+    }
+})
