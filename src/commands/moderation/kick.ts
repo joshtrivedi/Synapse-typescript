31d99cfb7960cd3d9dcb9d405de87be8bb9f245a
Josh Trivedi <josh.trivedi@videonetics.com>
2022-05-22 15:54:44 +0530

    chore: fixed role precedence in commands

--- src/commands/moderation/kick.ts
+++ src/commands/moderation/kick.ts
@@ -1,49 +1,50 @@
-import { Command } from '../../structures/Command'
-import Discord, { Permissions } from 'discord.js'
-import { isAuthorModerator } from '../../permissionsHandler'
-import { embedCreate } from '../../structures/EmbedCreate'
-
-
+import { Permissions } from "discord.js";
+import { Command } from "../../structures/Command";
+import { embedCreate } from "../../structures/EmbedCreate";
+import Discord from "discord.js";
 export default new Command({
-    name: "ban",
-    description: "Ban a member from this server.",
+    name: "kick",
+    description: "Kick a member from this server.",
     options: [
         {
             name: "user",
-            description: `User tag to ban`,
+            description: `User tag to kick`,
             required: true,
             type: "USER",
         },
         {
             name: 'reason',
-            description: `Put in the reason for the ban`,
+            description: `Put in the reason for the kick`,
             required: true,
             type: Discord.Constants.ApplicationCommandOptionTypes.STRING,
         }
     ],
     run: async ({ client, interaction }) => {
-        //check permission to ban members
-        if(!interaction.member.permissions.has(Permissions.FLAGS.BAN_MEMBERS)) {
+        //check permission to kick members
+        if(!interaction.member.permissions.has(Permissions.FLAGS.KICK_MEMBERS)) {
             return interaction.followUp("You do not have required permissions to use this command")
         }
         try {
+            
             const reason = interaction.options.getString('reason')
             const user = interaction.options.getUser('user')
             const member = interaction.guild.members.cache.get(user.id)
 
             let u = interaction.guild.members.cache.get(user.id)
             if(u.permissions.has(Permissions.FLAGS.BAN_MEMBERS || Permissions.FLAGS.KICK_MEMBERS)){
-                return interaction.followUp("You cannot ban another moderator or owner")
+                return interaction.followUp("You cannot kick another moderator or owner")
             }
 
-            const data = [`${user.tag}`, ` Has been Banned for ${reason}`]
+            const data = [`${user.tag}`, ` Has been Kicked for ${reason}`]
             const embed = embedCreate(data, [], [])
             interaction.followUp({ embeds: [embed] })
-            await member.user.send(`You are banned from **\`${interaction.guild.name}\`** for \`${reason}\``)
-            try{member.ban({ reason })}catch(e){
-                console.log(e) 
+            await member.user.send(`You have been kicked from **\`${interaction.guild.name}\`** for \`${reason}\``)
+            try{member.kick(reason)}
+            catch(e){
+                console.log(e)
                 interaction.followUp("Please check the Role precedence")
             }
+           
         } catch (e) {
             console.log(e)
             interaction.followUp("Please check the bot permissions ")
