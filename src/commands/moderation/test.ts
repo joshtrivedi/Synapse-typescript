0685737ee70040e2f23deb3ed9485b6167702875
Josh Trivedi <josh.trivedi@videonetics.com>
2022-05-24 12:11:57 +0530

    purge reset

--- src/commands/moderation/test.ts
+++ src/commands/moderation/test.ts
@@ -1,22 +1,22 @@
-import { Command } from '../../structures/Command'
-import Discord, { Message, MessageSelectMenu } from 'discord.js'
-import { isAuthorModerator } from '../../permissionsHandler'
-import { embedCreate } from '../../structures/EmbedCreate'
-import axios from "axios"
-const simBriefToken = process.env.SIMBRIEF_TOKEN
-const api = `api.simbox.org/ofp/?token=${simBriefToken}&username=lauder`
+// import { Command } from '../../structures/Command'
+// import Discord, { Message, MessageSelectMenu } from 'discord.js'
+// import { isAuthorModerator } from '../../permissionsHandler'
+// import { embedCreate } from '../../structures/EmbedCreate'
+// import axios from "axios"
+// const simBriefToken = process.env.SIMBRIEF_TOKEN
+// const api = `api.simbox.org/ofp/?token=${simBriefToken}&username=lauder`
 
 
-export default new Command({
-    name: "reset",
-    description: "Reset guild commands",
-    run: async ({ client, interaction }) => {
-        if(!isAuthorModerator(interaction.member)) {
-            return interaction.followUp("You do not have permission to use this command.")
-        }
-        client.application.commands.set([])
-        .then(() => {
-            interaction.followUp("Global Commands reset.")
-        })
-    }
-}) 
+// export default new Command({
+//     name: "reset",
+//     description: "Reset guild commands",
+//     run: async ({ client, interaction }) => {
+//         if(!isAuthorModerator(interaction.member)) {
+//             return interaction.followUp("You do not have permission to use this command.")
+//         }
+//         client.application.commands.set([])
+//         .then(() => {
+//             interaction.followUp("Global Commands reset.")
+//         })
+//     }
+// }) 
